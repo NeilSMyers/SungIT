b7f41249aa1b56aa1aaf7bf05422b7ea7283fa33
neilmyers <mmyersneil@gmail.com>
2019-10-17 15:50:41 -0600

    intro screen with play button

--- screens/Play.js
+++ screens/Play.js
@@ -1,14 +1,54 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 
 const Play = props => {
   return (
-    <View>
-      <Text>Play Screen</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.intro}>
+        <Text style={styles.title}>SungIT</Text>
+        <Text style={styles.text}>Hear it, Guess it, Go again</Text>
+        <Text style={styles.text}>How many can you get in a row?</Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("Game")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>PLAY</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#393d3f"
+  },
+  title: {
+    fontSize: 50,
+    color: "#fdfdff"
+  },
+  intro: {
+    alignItems: "center"
+  },
+  button: {
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "#c6c5b9",
+    borderRadius: 50,
+    padding: 20,
+    width: "75%",
+    backgroundColor: "#62929e"
+  },
+  buttonText: {
+    fontSize: 30,
+    color: "#fdfdff"
+  },
+  text: {
+    color: "#fdfdff"
+  }
+})
 
 export default Play
